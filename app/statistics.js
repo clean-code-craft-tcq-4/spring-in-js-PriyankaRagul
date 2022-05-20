1c0677ddd9c7bcddd9f522b6ad80456b07e9377c
PriyankaRagul <105204385+PriyankaRagul@users.noreply.github.com>
2022-05-20 17:20:30 +0530

    Update statistics.js

--- app/statistics.js
+++ app/statistics.js
@@ -1,5 +1,10 @@
 module.exports=function getStatistics(numbers){
     
     //implement the computation of statistics here
+    const arrMin = Math.min(...numbers);
+   	const arrMax = Math.max(...numbers);
+	const arrAvg = numbers.reduce((a,b) => a + b, 0) / numbers.length;
+    let output ={"min":arrMin,"max":arrMax,"average":arrAvg}
+    return output;
 }
 
